da6a10d1899a7cb68a42b6965138e13249284f46
Lily <190546707+SkibidiWatesiggma@users.noreply.github.com>
2026-02-06 11:12:33 +0100

    Captive Portal Login implementation in Kotlin.

---
+++ CaptivePortalActivity.kt
@@ -0,0 +1,37 @@
+package com.google.android.captiveportallogin
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.content.Intent
+import android.util.Log
+
+class CaptivePortalActivity : Activity() {
+override fun onCreate(savedInstanceState: Bundle?){
+setTheme(android.R.style.Theme_NoTitleBar_Fullscreen)
+
+super.onCreate(savedInstanceState)
+
+val webView = WebView(this)
+
+webView.webViewClient = WebViewClient()
+
+webView.settings.javaScriptEnabled = true
+
+setContentView(webView)
+
+val captivePortalUrl = intent.dataString?: intent.getStringExtra("android.net.extra.CAPTIVE_PORTAL_URL")
+
+if (captivePortalUrl != null) {
+webView.loadUrl(captivePortalUrl)
+
+} else {
+Log.d("CaptivePortalActivity", "Captive Portal URL not found. Please contact your network administrator.")
+
+}
+
+
+}
+
+}
